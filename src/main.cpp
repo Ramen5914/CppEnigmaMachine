a6174461436b88cc4fb3c05db70708cc412b036e
Omar Rahman <omar@r4men.com>
2025-04-20 05:51:42 -0400

    Starting to flesh out the CLI

--- src/main.cpp
+++ src/main.cpp
@@ -14,9 +14,38 @@ using namespace std;
 
 int main(int argc, char** argv) {
     CLI::App app{"A feature-rich software implementation of the Enigma Machine."};
-    fs::path userDataDir = FileUtils::getUserDataDirectory("enigma");
+
+    // Rotor
+    CLI::App* rotorSubCmd = app.add_subcommand("rotor");
+
+    CLI::App* rotorAddSubCmd = rotorSubCmd->add_subcommand("add");
+    std::string rotor_name;
+    rotorAddSubCmd->add_option("name", rotor_name, "Rotor name")->required();
+    int notches = 1;
+    rotorAddSubCmd->add_option("-n,--notches", notches, "Amount of notches on the rotor");
+    int seed;
+    rotorAddSubCmd->add_option("-s,--seed", seed, "Seed to use for generating rotor");
+
+    CLI::App* rotorRmSubCmd = rotorSubCmd->add_subcommand("rm");
+    CLI::App* rotorListSubCmd = rotorSubCmd->add_subcommand("list");
+
+    // Keylist
+    CLI::App* keylistSubCmd = app.add_subcommand("keylist");
+
+    CLI::App* keylistAddSubCmd = keylistSubCmd->add_subcommand("add");
+    CLI::App* keylistRmSubCmd = keylistSubCmd->add_subcommand("rm");
+    CLI::App* keylistListSubCmd = keylistSubCmd->add_subcommand("list");
+
+    // Reflector
+    CLI::App* reflectorSubCmd = app.add_subcommand("reflector");
+
+    CLI::App* reflectorAddSubCmd = reflectorSubCmd->add_subcommand("add");
+    CLI::App* reflectorRmSubCmd = reflectorSubCmd->add_subcommand("rm");
+    CLI::App* reflectorListSubCmd = reflectorSubCmd->add_subcommand("list");
 
     CLI11_PARSE(app, argc, argv);
+
+    fs::path userDataDir = FileUtils::getUserDataDirectory("enigma");
 
     return 0;
 }
